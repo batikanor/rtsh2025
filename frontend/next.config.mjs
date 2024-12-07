3a03b808f7de0cd34aedba40a2efc2f482963d8e
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2024-12-07 22:20:37 +0100

    Minor fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
     basePath: "/rtsh2025",
-    assetPrefix: "/rtsh2025/", // Add this line
+    assetPrefix: "/rtsh2025", // Add this line
 
     /**
      * Disable server-based image optimization. Next.js does not support
