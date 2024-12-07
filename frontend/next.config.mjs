9be0281c6184c9f350895a4c19f2cd4fec26b0f3
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2024-12-07 10:42:56 +0100

    Create frontend

---
+++ frontend/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    // If deploying to a repo at https://username.github.io/repo-name/
+    // uncomment and set these paths accordingly:
+    // basePath: '/repo-name',
+    // assetPrefix: '/repo-name/'
+  };
+  
+  export default nextConfig;
+  
